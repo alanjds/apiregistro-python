33c23f9d7e8ad270c1896dab4a5aeac1d340e86b
Alan Justino <alan.justino@yahoo.com.br>
2017-07-23 21:25:38 -0300

    Client is nice on the REPL

--- apiregistro/client.py
+++ apiregistro/client.py
@@ -1,21 +1,36 @@
 # coding: utf-8
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+import six
+from future.utils import python_2_unicode_compatible
+
 import logging
 logger = logging.getLogger(__name__)
 
 import slumber
 
 
+@python_2_unicode_compatible
 class Client(object):
     def __init__(self, token=None, base_url='https://www.apiregistro.com.br:80', document=None):
-        self.token = token
-        self.document = document
+        self._token = token
+        self._document = document
+        self._base_url = base_url
         self._slumber = slumber.API(base_url + '/api/v1')
         self._slumber._store['session'].headers.update({
             'Token': token,
         })
 
-    def __getattr__(self, name):
-        return getattr(self._slumber, name)
+    def __repr__(self):
+        try:
+            u = six.text_type(self)
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            u = '[Bad Unicode data]'
+        return '<%s: %s>' % (self.__class__.__name__, u)
+
+    def __str__(self):
+        return 'token=%s base=%s' % (self._token, self._base_url)
 
     def domain_search(self, name):
         return self._slumber.domains.get(search=name)['results']
