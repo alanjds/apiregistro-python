6cf87cdce3e35291a702fc763fff791fa1976eeb
Alan Justino <alan.justino@yahoo.com.br>
2017-07-23 21:11:56 -0300

    slumber-based w/ headers hack

--- apiregistro/client.py
+++ apiregistro/client.py
@@ -2,6 +2,33 @@
 import logging
 logger = logging.getLogger(__name__)
 
+import slumber
+
+
 class Client(object):
-    def __init__(self, token):
+    def __init__(self, token=None, base_url='https://www.apiregistro.com.br:80', document=None):
         self.token = token
+        self.document = document
+        self._slumber = slumber.API(base_url + '/api/v1')
+        self._slumber._store['session'].headers.update({
+            'Token': token,
+        })
+
+    def __getattr__(self, name):
+        return getattr(self._slumber, name)
+
+    def domain_search(self, name):
+        return self._slumber.domains.get(search=name)['results']
+
+    def domain_info(self, name):
+        return self._slumber.domains(name)
+
+    def domain_buy(self, name, document=None):
+        document = document or self.document
+        if not document:
+            raise RuntimeError("Cannot register a domain without a 'document'")
+
+        result = self._slumber.domains(name).buy.post({
+            'document': document,
+        })
+        return result
